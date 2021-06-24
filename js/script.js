ec843260eae3745d441316dfb8af329a67d7591d
Homam Atar <homam.attar@gmail.com>
2021-06-24 19:25:01 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-var x ="Hello World"
+// var company = new Object();
+// company.namy = "Qaws";
+// company.ceo = new Object();
+// company.ceo.name = "Abd";
+// company.ceo.carcolor = "White";
+// console.log(company);
+
+var company = {
+	name : "Qaws",
+	ceo : {
+		firstname : "Abd",
+		carcolor: "White"
+	},
+	"stock of company": 110
+};
+
+console.log(company.ceo.firstname);
